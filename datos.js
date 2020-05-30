52a862e5b6f18d8d0d010f4c5567e04e421fd7d1
Leopnardo Espitia <leonardo041@hotmail.com>
2020-05-29 22:52:55 -0500

    uno

---
+++ datos.js
@@ -0,0 +1,6 @@
+function recoger(){
+var base = document.getElementById ("base").value;
+var altura = document.getElementById ("altura").value;
+var area = base * altura;
+alert('El Área del Triangulo es: '+area);
+}
